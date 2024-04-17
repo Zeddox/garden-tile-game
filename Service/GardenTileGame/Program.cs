e27c92d42e550bd486f184a8cfff79180959f8fa
Andrew Joung <andrewjoung@microsoft.com>
2024-04-16 21:13:41 -0700

    dotnet setup

---
+++ Service/GardenTileGame/Program.cs
@@ -0,0 +1,54 @@
+
+using GardenTileGame.Data;
+
+namespace GardenTileGame.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using IServiceScope scope = host.Services.CreateScope();
+
+            try
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<GardenTileGameDbContext>();
+                dbContext.Database.EnsureCreated();
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                scope.ServiceProvider.GetRequiredService<ILogger<Program>>()
+                    .LogCritical(ex, ex.Message);
+                throw;
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host
+                .CreateDefaultBuilder(args)
+                .ConfigureLogging(loggingBuilder =>
+                {
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.AddConsole();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    var env = webBuilder.GetSetting("ENVIRONMENT");
+                    if (env == null)
+                    {
+                        throw new Exception("Environment variable ASPNETCORE_ENVIRONMENT cannot be null or empty");
+                    }
+
+                    webBuilder.ConfigureKestrel(serverOptions => serverOptions.AddServerHeader = false);
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration((hostBuilder, configBuilder) =>
+                {
+                    configBuilder.AddUserSecrets<Startup>();
+                    configBuilder.Build();
+                });
+        }
+    }
+}
---
+++ Service/GardenTileGame/Startup.cs
@@ -0,0 +1,67 @@
+﻿using GardenTileGame.API.Infrastructure;
+using GardenTileGame.Data;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace GardenTileGame.API;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+    private readonly IHostEnvironment _environment;
+
+    public Startup(IConfiguration configuration, IHostEnvironment environment)
+    {
+        _configuration = configuration;
+        _environment = environment;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services
+            .AddMvc(options =>
+            {
+                options.Conventions.Add(new RouteTokenTransformerConvention(new SlugifyParameterTransformer()));
+            });
+
+        services
+            .AddResponseCompression()
+            .AddDbConnections<GardenTileGameDbContext>(_configuration, _environment.EnvironmentName)
+            .AddServices(_configuration)
+            .AddProblemDetails()
+            .AddCors(_configuration)
+            .AddOpenApi(1, "CAMP API")
+            .AddMvcWithOptions()
+            .AddControllersAsServices()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+            });
+    }
+    public void Configure(IApplicationBuilder app)
+    {
+        app.ConfigureSecurityHeaders();
+        app
+            .UseSecurityHeaders()
+            .RouteNonApiCallsToClient()
+            //.UseHeaderPropagation()
+            .UseResponseCompression()
+            .UseFileServer()
+            .UseOpenApi()
+            .UseSwaggerUi(config =>
+            {
+                config.AdditionalSettings.Add("displayRequestDuration", true);
+                config.TransformToExternalPath = (url, request) =>
+                {
+                    // stupid hack to get the UI to properly find the relative path of the swagger json instead of absolute path.
+                    return url.EndsWith(".json") || request.Path.ToString().EndsWith("/") ? ".." + url : request.PathBase + "." + url;
+                };
+            }).UseRouting()
+            .UseAuthentication()
+            .UseAuthorization()
+            .UseCors("CorsPolicy")
+            .UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+    }
+}
