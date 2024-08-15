4f207a15a44a39e690a892036ba4afa8653695b9
Brandon Simmons <v-brsimm@microsoft.com>
2024-08-15 13:32:44 -0700

    Fix for no user selected state

--- client/src/AppIndex.tsx
+++ client/src/AppIndex.tsx
@@ -13,13 +13,13 @@ export const AppIndex = () => {
 
     return (
         <div className={'grid grid-cols-10 gap-8'}>
-            {selectedUserId !== undefined ? (
+            {selectedUserId !== null ? (
                 <>
                     <GameList />
                     <GameCreationDialog />
                 </>
             ) : (
-                <h2 className={'text-2xl font-semibold text-slate-300'}>{'Select a user to begin ↑'}</h2>
+                <h2 className={'col-[-1] text-2xl font-semibold text-slate-300'}>{'Select a user to begin ↑'}</h2>
             )}
         </div>
     );
@@ -29,22 +29,21 @@ const GameList = () => {
     const selectedUserId = useAtomValue(useContext(AppContext)!.selectedUserIdAtom);
     const { data: games } = useGames();
 
-
     return (games?.length ?? 0) > 0 ? (
         <>
             <Card
                 className={
-                    'col-[2_/_6] flex flex-col gap-4 p-4  shadow-slate-950 shadow-md border-slate-700/40 bg-gradient-to-b from-slate-700/30 from-10% via-[#181b1a] via-60% to-[#181b1a] to-100%'
+                    'col-[2_/_6] flex flex-col gap-4 border-slate-700/40 bg-gradient-to-b from-slate-700/30 from-10% via-[#181b1a] via-60% to-[#181b1a] to-100% p-4 shadow-md shadow-slate-950'
                 }
             >
                 <h2 className={'text-2xl font-semibold text-slate-300'}>{'My Games'}</h2>
                 <div className={'flex flex-col gap-1'}>
                     {games
-                        ?.filter((x) => x.players?.find((x) => (x.userId === selectedUserId)) !== undefined)
+                        ?.filter((x) => x.players?.find((x) => x.userId === selectedUserId) !== undefined)
                         ?.map((x) => (
                             <div
                                 className={
-                                    'flex gap-2 p-1 pl-2 pr-2 items-center justify-between hover:shadow-slate-950 hover:shadow-sm hover:bg-emerald-800 rounded-sm group '
+                                    'group flex items-center justify-between gap-2 rounded-sm p-1 pl-2 pr-2 hover:bg-emerald-800 hover:shadow-sm hover:shadow-slate-950'
                                 }
                                 key={x.id}
                             >
@@ -62,17 +61,17 @@ const GameList = () => {
             </Card>
             <Card
                 className={
-                    'col-[6_/_10] flex flex-col gap-4 p-4  shadow-slate-950 shadow-md border-slate-700/40 bg-gradient-to-b from-slate-700/30 from-10% via-[#181b1a] via-60% to-[#181b1a] to-100%'
+                    'col-[6_/_10] flex flex-col gap-4 border-slate-700/40 bg-gradient-to-b from-slate-700/30 from-10% via-[#181b1a] via-60% to-[#181b1a] to-100% p-4 shadow-md shadow-slate-950'
                 }
             >
                 <h2 className={'text-2xl font-semibold text-slate-300'}>{'Games to Join'}</h2>
                 <div className={'flex flex-col gap-1'}>
                     {games
-                        ?.filter((x) => x.players?.find((x) => (x.userId === selectedUserId)) === undefined)
+                        ?.filter((x) => x.players?.find((x) => x.userId === selectedUserId) === undefined)
                         ?.map((x) => (
                             <div
                                 className={
-                                    'flex gap-2 p-1 pl-2 pr-2 items-center justify-between hover:shadow-slate-950 hover:shadow-sm hover:bg-primary rounded-sm group '
+                                    'group flex items-center justify-between gap-2 rounded-sm p-1 pl-2 pr-2 hover:bg-primary hover:shadow-sm hover:shadow-slate-950'
                                 }
                                 key={x.id}
                             >
