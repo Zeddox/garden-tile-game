a1652e2a513b8a75aac5b6a1f1f98a5a30000e5f
Zeddox <brandon@mistymtfam.com>
2025-04-21 20:31:05 -0700

    Adding additional game summary stats, most/second most and fifth layer bonuses

--- client/src/gameRoom/GameSummarySection.tsx
+++ client/src/gameRoom/GameSummarySection.tsx
@@ -2,6 +2,7 @@ import { useAtomValue } from 'jotai';
 import { useMemo } from 'react';
 import { getFifthLayerBonusAmount, getGameBoardCellsFromPlayerTurns, getRoundTiles, getTileMap } from './game';
 import { useGameRoomContext } from './useGameRoomContext';
+import { GiFlowers, GiLindenLeaf, GiCirclingFish, GiFruitTree, GiMushroomHouse, GiStonePile } from 'react-icons/gi';
 
 export const GameSummarySection = () => {
     const { gameAtom, fifthLayerBonusesAtom } = useGameRoomContext();
@@ -41,22 +42,75 @@ export const GameSummarySection = () => {
         return visibleTypeQuantitiesMap;
     }, [game, players]);
 
+    const { mostQuantityBonuses, secondMostQuantityBonuses } = useMemo(() => {
+        const mostQuantityBonuses = new Map<number, { quantity: number; playerIds: string[] }>();
+        const secondMostQuantityBonuses = new Map<number, { quantity: number; playerIds: string[] }>();
+
+        const sortedTypeQuantitiesMap = new Map<number, { quantity: number; playerId: string }[]>();
+        visibleTypeQuantitiesMap.forEach((playerQuantities, playerId) => {
+            playerQuantities.forEach((quantity, index) => {
+                if (sortedTypeQuantitiesMap.get(index) === undefined) {
+                    sortedTypeQuantitiesMap.set(index, [{ quantity, playerId }]);
+                } else {
+                    sortedTypeQuantitiesMap.get(index)!.push({ quantity, playerId });
+                }
+            });
+        });
+
+        sortedTypeQuantitiesMap.forEach((playerQuantities, index) => {
+            playerQuantities.sort((a, b) => b.quantity - a.quantity);
+            mostQuantityBonuses.set(index, {
+                quantity: playerQuantities[0].quantity,
+                playerIds: playerQuantities.filter((x) => x.quantity === playerQuantities[0].quantity).map((x) => x.playerId)
+            });
+            const awardSecondMostBonuses = !(mostQuantityBonuses.get(index)!.playerIds.length > 1);
+            if (awardSecondMostBonuses) {
+                const secondMostQuantity = playerQuantities.filter(
+                    (x) => x.quantity < mostQuantityBonuses.get(index)!.quantity
+                )[0].quantity;
+                secondMostQuantityBonuses.set(index, {
+                    quantity: secondMostQuantity,
+                    playerIds: playerQuantities.filter((x) => x.quantity === secondMostQuantity).map((x) => x.playerId)
+                });
+            }
+        });
+        return { mostQuantityBonuses, secondMostQuantityBonuses };
+    }, [visibleTypeQuantitiesMap]);
+
     return (
         <div className={'bg-slate w-1/4 flex-auto border-slate-700/40'}>
             <span className={'text-xl'}>{'Players'}</span>
             <div className={'mt-4 grid auto-rows-auto'}>
-                <div>
+                <div className={'grid grid-cols-5 gap-2 font-semibold text-slate-400'}>
                     <div>{'Player'}</div>
                     <div>{'Final Score'}</div>
                     <div>{'Fifth Layer Bonuses'}</div>
                     <div>{'Most Type Bonuses'}</div>
                     <div>{'Rounds Points'}</div>
                 </div>
+                <div className={'grid grid-cols-5 gap-2 font-semibold text-slate-400'}>
+                    <div></div>
+                    <div></div>
+                    <div className={'grid h-4 grid-cols-6 items-center gap-2'}>
+                        <GiFruitTree />
+                        <GiMushroomHouse />
+                        <GiCirclingFish />
+                        <GiFlowers />
+                        <GiLindenLeaf />
+                        <GiStonePile />
+                    </div>
+                    <div className={'grid h-4 grid-cols-6 items-center gap-2'}>
+                        <GiFruitTree />
+                        <GiMushroomHouse />
+                        <GiCirclingFish />
+                        <GiFlowers />
+                        <GiLindenLeaf />
+                        <GiStonePile />
+                    </div>
+                    <div></div>
+                </div>
                 {players.map((player) => (
-                    <div
-                        key={player.id}
-                        className={'flex justify-between p-1 px-3 data-[is-current-player="true"]:bg-[--primary-50]'}
-                    >
+                    <div key={player.id} className={'grid grid-cols-5 gap-2 data-[is-current-player="true"]:bg-[--primary-50]'}>
                         <div className={'flex items-center gap-3'}>
                             <span className={'h-4 w-4 rounded-sm'} style={{ background: player.gamePieceColor }}></span>
                             <span>{player.name}</span>
@@ -64,15 +118,38 @@ export const GameSummarySection = () => {
                         <div className={'flex items-center gap-2'}>
                             <div className={'font-semibold'}>{scores.get(player.id) ?? 0}</div>
                         </div>
-                        <div className={'flex items-center gap-2'}>
-                            {/* <div className={'font-semibold'}>{scores.get(player.id) ?? 0}</div> */}
+                        <div className={'grid grid-cols-6 items-center gap-2'}>
+                            {Array.from({ length: 6 }).map((_, index) => {
+                                const bonus = fifthLayerBonuses.find((x) => x.playerId === player.id && x.tileType === index);
+                                if (bonus) {
+                                    return (
+                                        <span key={`${player.id}_${index}`} className={'text-stone-500'}>
+                                            {getFifthLayerBonusAmount(bonus.tileType)}
+                                        </span>
+                                    );
+                                }
+                                return <span key={`${player.id}_${index}`}></span>;
+                            })}
                         </div>
-                        <div className={'flex items-center gap-2'}>
-                            {/* <div className={'font-semibold'}>{scores.get(player.id) ?? 0}</div>
-                             */}
-                            {visibleTypeQuantitiesMap.get(player.id)!.map((x) => (
-                                <span key={`${player.id}_${x}`}>{x}</span>
-                            ))}
+                        <div className={'grid grid-cols-6 items-center gap-2'}>
+                            {Array.from({ length: 6 }).map((_, index) => {
+                                const most = mostQuantityBonuses.get(index);
+                                const secondMost = secondMostQuantityBonuses.get(index);
+                                if (most?.playerIds.includes(player.id) && most.quantity > 0) {
+                                    return (
+                                        <span key={`${player.id}_${index}`} className={'text-amber-400'}>
+                                            {most.quantity}
+                                        </span>
+                                    );
+                                } else if (secondMost?.playerIds.includes(player.id) && secondMost.quantity > 0) {
+                                    return (
+                                        <span key={`${player.id}_${index}`} className={'text-zinc-300'}>
+                                            {secondMost.quantity}
+                                        </span>
+                                    );
+                                }
+                                return <span key={`${player.id}_${index}`}></span>;
+                            })}
                         </div>
                         <div className={'flex items-center gap-2'}>
                             {/* <div className={'font-semibold'}>{scores.get(player.id) ?? 0}</div> */}
